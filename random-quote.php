0ac004ae742994e31ab90bb4f50f7f3f985c0f5b
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-02 14:44:06 -0600

    Update random-quote.php

--- random-quote.php
+++ random-quote.php
@@ -11,10 +11,12 @@ include "view-header.php";
       <script>
             function generateRandomQuote() {
                 const quotes = [
-                    "The best way to predict the future is to create it.",
-                    "Success is not the key to happiness. Happiness is the key to success.",
-                    "Do what you can with what you have where you are.",
-                    "The only limit to our realization of tomorrow is our doubts of today."
+                  "The best way to predict the future is to create it.",
+                  "Success is not the key to happiness. Happiness is the key to success.",
+                  "Do what you can with what you have where you are.",
+                  "The only limit to our realization of tomorrow is our doubts of today.",
+                  "Defining and analyzing humor is a pastime of humorless people.",
+                  "The most important thing in communication is to hear what isn't being said."
                 ];
                 const randomIndex = Math.floor(Math.random() * quotes.length);
                 document.getElementById('quote-display').innerText = quotes[randomIndex];
