348f9e53068e4fbbb89ee86ebb7c5ad21715429b
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-02 14:56:15 -0600

    Update page5.php

--- page5.php
+++ page5.php
@@ -2,9 +2,29 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-      
-      <h1>Page 5</h1>
-      
+
+<body>
+      <div id="drag-item" draggable="true" ondragstart="drag(event)" style="width: 100px; height: 100px; background: red;"></div>
+      <div ondrop="drop(event)" ondragover="allowDrop(event)" style="width: 200px; height: 200px; border: 1px dashed #000; margin-top: 20px;"></div>
+
+      <script>
+      function allowDrop(event) {
+          event.preventDefault();
+      }
+
+      function drag(event) {
+          event.dataTransfer.setData("text", event.target.id);
+      }
+
+      function drop(event) {
+          event.preventDefault();
+          const data = event.dataTransfer.getData("text");
+          event.target.appendChild(document.getElementById(data));
+      }
+
+      </script>
+</body>
+
 <?php
 include "view-footer.php";
 ?>
