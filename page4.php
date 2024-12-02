602308a1a3f1ab929a2874807b2e113eaa30519f
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-02 14:58:05 -0600

    Update page4.php

--- page4.php
+++ page4.php
@@ -2,9 +2,20 @@
 $pageTitle = "Home";
 include "view-header.php";
 ?>
-      
-      <h1>Page 4</h1>
-      
+
+<body>
+      <H1>Color Picker</H1>
+      <input type="color" id="color-picker" class="form-control" onchange="changeBackgroundColor()">
+      <div id="color-box" style="width: 100px; height: 100px; border: 1px solid #000; margin-top: 10px;"></div>
+
+      <script>
+      function changeBackgroundColor() {
+          const color = document.getElementById('color-picker').value;
+          document.getElementById('color-box').style.backgroundColor = color;
+      }
+      </script>
+</body>
+
 <?php
 include "view-footer.php";
 ?>
