42b78511c13d887d8616df33c6af55a3a3ce3acc
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-02 15:51:41 -0600

    Update image-zoom.php

--- image-zoom.php
+++ image-zoom.php
@@ -3,8 +3,8 @@ $pageTitle = "Image Zoom";
 include "view-header.php";
 ?>
 
-<body class="text-center p-5" style="margin-top:10px">
-    <img src="https://images.sidearmdev.com/resize?url=https%3A%2F%2Fdxbhsrqyrr690.cloudfront.net%2Fsidearm.nextgen.sites%2Fsoonersports.com%2Fimages%2F2021%2F5%2F6%2Fstadium.jpg&height=300&type=webp" alt="Hover to enlarge" class="hover-effect">
+<body class="text-center p-5">
+    <img src="https://images.sidearmdev.com/resize?url=https%3A%2F%2Fdxbhsrqyrr690.cloudfront.net%2Fsidearm.nextgen.sites%2Fsoonersports.com%2Fimages%2F2021%2F5%2F6%2Fstadium.jpg&height=300&type=webp" alt="Hover to enlarge" class="hover-effect" style="margin-top:10px">
 </body>
 
 <?php
