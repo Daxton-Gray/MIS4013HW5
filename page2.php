09d6e16a641b2aaeac611952fbfc405cab79a1bf
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-02 14:34:38 -0600

    Update page2.php

--- page2.php
+++ page2.php
@@ -3,8 +3,22 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
       
-      <h1>Page 2</h1>
-      
+<h1>Random Quote</h1>
+<p id="quote-display">Click the button for a random quote!</p>
+<button class="btn btn-warning" onclick="generateRandomQuote()">Get Quote</button>
+
+function generateRandomQuote() {
+    const quotes = [
+        "The best way to predict the future is to create it.",
+        "Success is not the key to happiness. Happiness is the key to success.",
+        "Do what you can with what you have where you are.",
+        "The only limit to our realization of tomorrow is our doubts of today."
+    ];
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    document.getElementById('quote-display').innerText = quotes[randomIndex];
+}
+
+
 <?php
 include "view-footer.php";
 ?>
