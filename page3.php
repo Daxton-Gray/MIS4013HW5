72b9a125037703f2306644a3ee7fea2dfe1e4010
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-02 14:46:11 -0600

    Update page3.php

--- page3.php
+++ page3.php
@@ -3,8 +3,23 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
       
-      <h1>Page 3</h1>
-      
+<body class="text-center p-5">
+   <h1>Random Quote</h1>
+      <p>Current Time: <span id="clock"></span></p>
+      <script>startClock();</script>
+
+      <script>
+            function startClock() {
+                  const clock = document.getElementById('clock');
+                  setInterval(() => {
+                        const now = new Date();
+                        clock.innerText = now.toLocaleTimeString();
+                  }, 1000);
+            }
+      </script>   
+</body>
+
+
 <?php
 include "view-footer.php";
 ?>
