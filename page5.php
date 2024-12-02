7f61abb6479c3841bb65d7a3d22cd067a2c5d0ee
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-02 15:36:42 -0600

    Update page5.php

--- page5.php
+++ page5.php
@@ -4,7 +4,7 @@ include "view-header.php";
 ?>
 
 <body class="text-center p-5">
-    <img src="https://via.placeholder.com/300" alt="Hover to enlarge" class="hover-effect">
+    <img src="https://preview.redd.it/cetpwklahlq61.jpg?width=1080&crop=smart&auto=webp&s=58431cda13c2d30ca1febaafb5f8fc34aba92936" alt="Hover to enlarge" class="hover-effect">
 </body>
 
 <?php
