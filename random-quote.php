4c48fd562668497fabb998ebf23be07a074a0526
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-02 14:38:37 -0600

    Update random-quote.php

--- random-quote.php
+++ random-quote.php
@@ -3,23 +3,25 @@ $pageTitle = "Home";
 include "view-header.php";
 ?>
       
-<h1>Random Quote</h1>
-<p id="quote-display">Click the button for a random quote!</p>
-<button class="btn btn-warning" onclick="generateRandomQuote()">Get Quote</button>
+<body>
+   <h1>Random Quote</h1>
+      <p id="quote-display">Click the button for a random quote!</p>
+      <button class="btn btn-warning" onclick="generateRandomQuote()">Get Quote</button>
 
-<script>
-      function generateRandomQuote() {
-          const quotes = [
-              "The best way to predict the future is to create it.",
-              "Success is not the key to happiness. Happiness is the key to success.",
-              "Do what you can with what you have where you are.",
-              "The only limit to our realization of tomorrow is our doubts of today."
-          ];
-          const randomIndex = Math.floor(Math.random() * quotes.length);
-          document.getElementById('quote-display').innerText = quotes[randomIndex];
-      }
+      <script>
+            function generateRandomQuote() {
+                const quotes = [
+                    "The best way to predict the future is to create it.",
+                    "Success is not the key to happiness. Happiness is the key to success.",
+                    "Do what you can with what you have where you are.",
+                    "The only limit to our realization of tomorrow is our doubts of today."
+                ];
+                const randomIndex = Math.floor(Math.random() * quotes.length);
+                document.getElementById('quote-display').innerText = quotes[randomIndex];
+            }
+      </script>   
+</body>
 
-</script>
 
 <?php
 include "view-footer.php";
