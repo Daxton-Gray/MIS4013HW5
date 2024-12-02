5f51930a9081cc1a5f6fa6e75f5753d889fdd740
Daxton-Gray <157337584+Daxton-Gray@users.noreply.github.com>
2024-12-02 14:46:46 -0600

    Update and rename page3.php to live-clock.php

--- live-clock.php
+++ live-clock.php
@@ -4,7 +4,7 @@ include "view-header.php";
 ?>
       
 <body class="text-center p-5">
-   <h1>Random Quote</h1>
+   <h1>Live Clock</h1>
       <p>Current Time: <span id="clock"></span></p>
       <script>startClock();</script>
 
